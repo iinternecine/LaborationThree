435eb529faae22161d67d85f5adf4137446d6a5f
iinternecine <my.senpaii@yandex.ru>
2024-11-17 14:11:59 +0300

    "The function of entering the number A has been added"

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number A: ");
+        double a = Convert.ToDouble(Console.ReadLine()); // Считываем число A
+        Console.WriteLine($"You have entered: {a}");
+    }
+}
