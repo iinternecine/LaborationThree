9e9ad1e92af48f53bfeae0905ff8bd12079bc8be
iinternecine <my.senpaii@yandex.ru>
2024-11-17 14:20:23 +0300

    "The function of entering the number addition has been added"

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number A: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter a number B: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"The result of addition: {a + b}");
+    }
+}
