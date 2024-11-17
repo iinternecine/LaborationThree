c9e19c4f25e9ab6dc307ac0b556ba8c4bd82a4db
iinternecine <my.senpaii@yandex.ru>
2024-11-17 14:17:56 +0300

    "The function of entering the number B has been added"

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number B: ");
+        double b = Convert.ToDouble(Console.ReadLine()); // Считываем число B
+        Console.WriteLine($"You have entered: {b}");
+    }
+}
